585be8387809142326c571d2f4de46aee79967e1
SoHaiNp <l.guinycholas@gmail.com>
2025-08-02 01:39:37 -0300

    Initial commit

---
+++ src/main/java/com/eccolimp/cacamba_manager/CacambaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.eccolimp.cacamba_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacambaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacambaManagerApplication.class, args);
+	}
+
+}
