73c8389ee2ef1107eccf4603f458f58ef5d0bd17
Gustavo Nycholas <94090147+gnycholas@users.noreply.github.com>
2025-08-05 13:46:51 -0300

    Adiciona teste de validação do CacambaController

--- src/main/java/com/eccolimp/cacamba_manager/dto/CacambaDTO.java
+++ src/main/java/com/eccolimp/cacamba_manager/dto/CacambaDTO.java
@@ -1,5 +1,13 @@
 package com.eccolimp.cacamba_manager.dto;
 
 import com.eccolimp.cacamba_manager.domain.model.StatusCacamba;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record CacambaDTO(Long id, String codigo, Integer capacidadeM3, StatusCacamba status) {}
+public record CacambaDTO(
+        Long id,
+        @NotBlank @Size(max = 10) String codigo,
+        @NotNull @Min(1) Integer capacidadeM3,
+        StatusCacamba status) {}
